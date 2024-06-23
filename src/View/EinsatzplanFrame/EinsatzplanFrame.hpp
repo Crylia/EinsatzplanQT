97e5a3faf78f1dbc50a9fa51592df555bf0799bc
AJ <aledonoah777@gmail.com>
2024-06-23 17:59:07 +0200

    added EinsatzplanFrame members

--- src/View/EinsatzplanFrame/EinsatzplanFrame.hpp
+++ src/View/EinsatzplanFrame/EinsatzplanFrame.hpp
@@ -1,7 +1,21 @@
 # include <QFrame>
+# include <QLabel>
+# include <QPushButton>
+# include "../PlanGrid/PlanGrid.hpp"
 
 class EinsatzplanFrame : public QFrame{
 protected:
+    QLabel* profileImg_m;
+    QLabel* id_m;
+    QLabel* einsatzplanLabel_m;
+    
+    PlanGrid* planGrid_m;
+    
+    QPushButton* abmeldenButton_m;
+    QPushButton* createMemberButton_m;
+    QPushButton* deleteMemberButton_m;
+    QPushButton* createVeranstaltungButton_m;
+    QPushButton* deleteVeranstaltungButton_m;
 
 public:
     EinsatzplanFrame(QWidget* parent = nullptr, bool admin);
---
+++ src/View/PlanGrid/PlanGrid.hpp
@@ -0,0 +1,7 @@
+# include <QWidget>
+
+class PlanGrid : QWidget{
+protected:
+public:
+    PlanGrid(QWidget* parent = nullptr);
+};
