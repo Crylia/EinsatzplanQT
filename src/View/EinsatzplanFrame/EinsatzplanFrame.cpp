5ec9f4a913cc7484f1ed5f6cdaf4db0937aeae42
Crylia <rene-schwarz-99@protonmail.com>
2024-06-24 22:51:15 +0200

    fix some styling

--- src/View/EinsatzplanFrame/EinsatzplanFrame.cpp
+++ src/View/EinsatzplanFrame/EinsatzplanFrame.cpp
@@ -1,11 +1,10 @@
 # include "EinsatzplanFrame.hpp"
 
 EinsatzplanFrame::EinsatzplanFrame(QWidget* parent, QString id, bool admin)
-:QFrame(parent)
-{
-    setFrameStyle(QFrame::Box);
-    setObjectName("einsatzplanFrame");
-    setStyleSheet(R"(
+	:QFrame(parent) {
+	setFrameStyle(QFrame::Box);
+	setObjectName("einsatzplanFrame");
+	setStyleSheet(R"(
     #einsatzplanFrame{
         background-color: #212121;
         border: none;
@@ -13,32 +12,32 @@ EinsatzplanFrame::EinsatzplanFrame(QWidget* parent, QString id, bool admin)
     )");
 
 
-    profileImg_m = new QLabel(this);
-    profileImg_m->setFixedSize(60,60);
-    profileImg_m->setObjectName("profileImg");
-    profileImg_m->setStyleSheet(R"(
+	profileImg_m = new QLabel(this);
+	profileImg_m->setFixedSize(60, 60);
+	profileImg_m->setObjectName("profileImg");
+	profileImg_m->setStyleSheet(R"(
     #profileImg{
         background-color: #000000;
     }
     )");
-    profileImg_m->show();
+	profileImg_m->show( );
 
 
-    id_m = new QLabel(id,this);
-    id_m->setFixedSize(122,80);
-    id_m->setObjectName("id");
-    id_m->setStyleSheet(R"(
+	id_m = new QLabel(id, this);
+	id_m->setFixedSize(122, 80);
+	id_m->setObjectName("id");
+	id_m->setStyleSheet(R"(
     #id{
     
     }
     )");
-    id_m->show();
+	id_m->show( );
 
 
-    abmeldenButton_m = new QPushButton("Abmelden",this);
-    abmeldenButton_m->setFixedSize(150,50);
-    abmeldenButton_m->setObjectName("abmeldenButton");
-    abmeldenButton_m->setStyleSheet(R"(
+	abmeldenButton_m = new QPushButton("Abmelden", this);
+	abmeldenButton_m->setFixedSize(150, 50);
+	abmeldenButton_m->setObjectName("abmeldenButton");
+	abmeldenButton_m->setStyleSheet(R"(
     #abmeldenButton{
         font-size: 24px;
         font-weight: bold;
@@ -50,120 +49,124 @@ EinsatzplanFrame::EinsatzplanFrame(QWidget* parent, QString id, bool admin)
         background-color: #D0793A;
     }
     )");
-    abmeldenButton_m->show();
+	abmeldenButton_m->show( );
 
 
-    einsatzplanLabel_m = new QLabel("Einsatzplan",this);
-    einsatzplanLabel_m->setFixedSize(645,80);
-    einsatzplanLabel_m->setObjectName("einsatzplanLabel");
-    einsatzplanLabel_m->setStyleSheet(R"(
+	einsatzplanLabel_m = new QLabel("Einsatzplan", this);
+	einsatzplanLabel_m->setFixedSize(645, 80);
+	einsatzplanLabel_m->setObjectName("einsatzplanLabel");
+	einsatzplanLabel_m->setStyleSheet(R"(
     #einsatzplanLabel{
     
     }
     )");
-    einsatzplanLabel_m->show();
+	einsatzplanLabel_m->show( );
 
-    
-    planGrid_m = new PlanGrid(this);
-    planGrid_m->show();
 
-    QVBoxLayout* totalLayout = new QVBoxLayout(this);
-    totalLayout->setContentsMargins(30,20,30,20);
+	planGrid_m = new PlanGrid(this);
+	planGrid_m->show( );
 
-    QHBoxLayout* topLayout = new QHBoxLayout(this);
-    topLayout->addWidget(profileImg_m, 1, Qt::AlignLeft);
-    topLayout->addWidget(id_m, 1, Qt::AlignLeft);
-    topLayout->addWidget(einsatzplanLabel_m, 7, Qt::AlignCenter);
-    topLayout->addWidget(abmeldenButton_m, 1, Qt::AlignRight);
+	QVBoxLayout* totalLayout = new QVBoxLayout(this);
+	totalLayout->setContentsMargins(30, 20, 30, 20);
 
-    QHBoxLayout* bottomLayout = new QHBoxLayout(this);
-    bottomLayout->addWidget(planGrid_m, 1, Qt::AlignCenter);
-    
-    
-    
+	QHBoxLayout* topLayout = new QHBoxLayout(this);
+	topLayout->addWidget(profileImg_m, 1, Qt::AlignLeft);
+	topLayout->addWidget(id_m, 1, Qt::AlignLeft);
+	topLayout->addWidget(einsatzplanLabel_m, 7, Qt::AlignCenter);
+	topLayout->addWidget(abmeldenButton_m, 1, Qt::AlignRight);
 
-    totalLayout->addLayout(topLayout);
-    totalLayout->addLayout(bottomLayout,2);
+	QHBoxLayout* bottomLayout = new QHBoxLayout(this);
+	bottomLayout->addWidget(planGrid_m, 1, Qt::AlignCenter);
 
 
 
-    if (admin){
 
-        createMemberButton_m = new QPushButton("Mitarbeiter\nHinzufügen",this);
-        createMemberButton_m->setFixedSize(200,50);
-        createMemberButton_m->setObjectName("createMember");
-        createMemberButton_m->setStyleSheet(R"(
+	totalLayout->addLayout(topLayout);
+	totalLayout->addLayout(bottomLayout, 2);
+
+
+
+	if (admin) {
+
+		createMemberButton_m = new QPushButton("Mitarbeiter\nHinzufügen", this);
+		createMemberButton_m->setFixedSize(200, 50);
+		createMemberButton_m->setObjectName("createMember");
+		createMemberButton_m->setStyleSheet(R"(
         #createMember{
             font-size: 16px;
             font-weight: bold;
             background-color: #DCFF78;
+						color: #212121;
             border-radius: 10px;
         }
         #createMember:pressed{
             background-color: #CCEF68;
         }
         )");
-        createMemberButton_m->show();
+		createMemberButton_m->show( );
 
 
-        deleteMemberButton_m = new QPushButton("Mitarbeiter\nEntfernen",this);
-        deleteMemberButton_m->setFixedSize(200,50);
-        deleteMemberButton_m->setObjectName("deleteMember");
-        deleteMemberButton_m->setStyleSheet(R"(
+		deleteMemberButton_m = new QPushButton("Mitarbeiter\nEntfernen", this);
+		deleteMemberButton_m->setFixedSize(200, 50);
+		deleteMemberButton_m->setObjectName("deleteMember");
+		deleteMemberButton_m->setStyleSheet(R"(
         #deleteMember{
             font-size: 16px;
             font-weight: bold;
             background-color: #DCFF78;
+						color: #212121;
             border-radius: 10px;
         }
         #deleteMember:pressed{
             background-color: #CCEF68;
         }
         )");
-        deleteMemberButton_m->show();
+		deleteMemberButton_m->show( );
 
 
-        createVeranstaltungButton_m = new QPushButton("Veranstaltung\nHinzufügen",this);
-        createVeranstaltungButton_m->setFixedSize(200,50);
-        createVeranstaltungButton_m->setObjectName("createVeranstaltung");
-        createVeranstaltungButton_m->setStyleSheet(R"(
+		createVeranstaltungButton_m = new QPushButton("Veranstaltung\nHinzufügen", this);
+		createVeranstaltungButton_m->setFixedSize(200, 50);
+		createVeranstaltungButton_m->setObjectName("createVeranstaltung");
+		createVeranstaltungButton_m->setStyleSheet(R"(
         #createVeranstaltung{
             font-size: 16px;
             font-weight: bold;
             background-color: #DCFF78;
+						color: #212121;
             border-radius: 10px;
         }
         #createVeranstaltung:pressed{
             background-color: #CCEF68;
         }
         )");
-        createVeranstaltungButton_m->show();
+		createVeranstaltungButton_m->show( );
 
 
-        deleteVeranstaltungButton_m = new QPushButton("Veranstaltung\nEntfernen",this);
-        deleteVeranstaltungButton_m->setFixedSize(200,50);
-        deleteVeranstaltungButton_m->setObjectName("deleteVeranstaltung");
-        deleteVeranstaltungButton_m->setStyleSheet(R"(
+		deleteVeranstaltungButton_m = new QPushButton("Veranstaltung\nEntfernen", this);
+		deleteVeranstaltungButton_m->setFixedSize(200, 50);
+		deleteVeranstaltungButton_m->setObjectName("deleteVeranstaltung");
+		deleteVeranstaltungButton_m->setStyleSheet(R"(
         #deleteVeranstaltung{
             font-size: 16px;
             font-weight: bold;
             background-color: #DCFF78;
+						color: #212121;
             border-radius: 10px;
         }
         #deleteVeranstaltung:pressed{
             background-color: #CCEF68;
         }
         )");
-        deleteVeranstaltungButton_m->show();
+		deleteVeranstaltungButton_m->show( );
 
-        QHBoxLayout* adminLayout = new QHBoxLayout(this);
-        adminLayout->addWidget(createMemberButton_m, 1, Qt::AlignLeft);
-        adminLayout->addWidget(deleteMemberButton_m,1 , Qt::AlignLeft);
-        adminLayout->addStretch(1);
-        adminLayout->addWidget(createVeranstaltungButton_m,1 , Qt::AlignRight);
-        adminLayout->addWidget(deleteVeranstaltungButton_m,1 , Qt::AlignRight);
-        
-        totalLayout->addLayout(adminLayout);
+		QHBoxLayout* adminLayout = new QHBoxLayout(this);
+		adminLayout->addWidget(createMemberButton_m, 1, Qt::AlignLeft);
+		adminLayout->addWidget(deleteMemberButton_m, 1, Qt::AlignLeft);
+		adminLayout->addStretch(1);
+		adminLayout->addWidget(createVeranstaltungButton_m, 1, Qt::AlignRight);
+		adminLayout->addWidget(deleteVeranstaltungButton_m, 1, Qt::AlignRight);
 
-    }
+		totalLayout->addLayout(adminLayout);
+
+	}
 }
--- src/View/PlanGrid/PlanGrid.cpp
+++ src/View/PlanGrid/PlanGrid.cpp
@@ -1,8 +1,7 @@
 # include "PlanGrid.hpp"
 
-PlanGrid::PlanGrid(QWidget* parent) 
-:QWidget(parent) 
-{
+PlanGrid::PlanGrid(QWidget* parent)
+	:QWidget(parent) {
 	setObjectName("PlanGrid");
 	setStyleSheet(R"(
 	QWidget{
@@ -36,67 +35,97 @@ PlanGrid::PlanGrid(QWidget* parent)
 
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 0; j < 10; ++j) {
-			QLabel* temp = new QLabel();
+			QLabel* temp = new QLabel( );
 			temp->setObjectName("temp");
 			temp->setStyleSheet(R"(
 			#temp{
 				
 			}
 			)");
-			temp->setFixedSize(250,40);
+			temp->setFixedSize(240, 40);
 			planMap->insert(qMakePair(weekdays[i], times[j]), temp);
 		}
 	}
 
-	populateGrid();
+	populateGrid( );
 
 
-	QLabel* temp = new QLabel();
+	QLabel* temp = new QLabel( );
 	temp->setObjectName("temp");
 	temp->setStyleSheet(R"(
 	#temp{
 		border-top-left-radius: 10px;
 	}
 	)");
-	temp->setFixedSize(140,80);
+	temp->setFixedSize(130, 80);
 	gridLayout->addWidget(temp, 0, 0);
 
 	for (int i = 0; i < 5; i++) {
 		QLabel* temp = new QLabel(weekdays[i]);
-		temp->setFixedSize(250,80);
+		temp->setFixedSize(240, 80);
 		temp->setObjectName("temp");
-		temp->setStyleSheet(R"(
-		#temp{
-			font-size: 24px;
-			font-weight: bold;
-			color: #d8d8d8;
+		if (i == 4) {
+			temp->setStyleSheet(R"(
+				#temp{
+					font-size: 24px;
+					font-weight: bold;
+					border-top-right-radius: 10px;
+					color: #d8d8d8;
+				}
+			)");
+
+		} else {
+			temp->setStyleSheet(R"(
+				#temp{
+					font-size: 24px;
+					font-weight: bold;
+					color: #d8d8d8;
+				}
+			)");
 		}
-		)");
-		gridLayout->addWidget(temp, 0, i+1,Qt::AlignCenter);
+		temp->setAlignment(Qt::AlignCenter);
+		gridLayout->addWidget(temp, 0, i + 1);
 	}
 
 	for (int i = 0; i < 10; i++) {
 		QLabel* temp = new QLabel(times[i]);
-		temp->setFixedSize(140,40);
+		temp->setFixedSize(130, 40);
 		temp->setObjectName("temp");
-		temp->setStyleSheet(R"(
-		#temp{
-			font-size: 16px;
-			font-weight: bold;
-			color: #d8d8d8;
+		if (i == 9) {
+			temp->setStyleSheet(R"(
+				#temp{
+					font-size: 16px;
+					font-weight: bold;
+					border-bottom-left-radius: 10px;
+					color: #d8d8d8;
+				}
+			)");
+		} else {
+			temp->setStyleSheet(R"(
+				#temp{
+					font-size: 16px;
+					font-weight: bold;
+					color: #d8d8d8;
+				}
+			)");
 		}
-		)");
-		gridLayout->addWidget(temp, i+1, 0, Qt::AlignCenter);
+		temp->setAlignment(Qt::AlignRight);
+		gridLayout->addWidget(temp, i + 1, 0);
 	}
 
 	gridLayout->setSpacing(0);
 	setLayout(gridLayout);
 }
 
-void PlanGrid::populateGrid(){
-	for (int i = 0; i<5; i++){
-		for (int j = 0; j < 10; ++j){
-			gridLayout->addWidget(planMap->value(qMakePair(weekdays[i],times[j])),j+1,i+1);
+void PlanGrid::populateGrid( ) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 10; ++j) {
+			gridLayout->addWidget(planMap->value(qMakePair(weekdays[i], times[j])), j + 1, i + 1);
+			if (i == 4 && j == 9) {
+				(planMap->value(qMakePair(weekdays[i], times[j])))->setStyleSheet(R"(
+					border-bottom-right-radius:10px;
+				)");
+			}
 		}
-	} 
+	}
 }
