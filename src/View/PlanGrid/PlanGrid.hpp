04e243682a7ebcd4c2a6dca7d16c8ad1257d15d5
Crylia <crylia@protonmail.com>
2024-06-24 09:48:43 +0200

    add first prototype of gridplanlayout

--- src/View/PlanGrid/PlanGrid.hpp
+++ src/View/PlanGrid/PlanGrid.hpp
@@ -1,9 +1,17 @@
 # pragma once
 # include <QGridLayout>
+# include <QWidget>
+# include <QLabel>
+# include <QDateTime>
 
-class PlanGrid {
+class PlanGrid : public QWidget {
+    Q_OBJECT
+private:
+    QString weekdays[5];
+    QString times[10];
 protected:
     QGridLayout* gridLayout;
+    QMap<QPair<QString, QString>, QLabel*>* planMap;
 public:
     PlanGrid(QWidget* parent = nullptr);
 };
