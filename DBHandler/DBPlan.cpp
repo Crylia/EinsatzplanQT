c01bba3459f50590c7adc87149db6b6bcda6cf88
Chris-bot374 <chris.moshoevel@stud.hn.de>
2024-07-01 17:49:30 +0200

    Update DBPlan.cpp

--- DBHandler/DBPlan.cpp
+++ DBHandler/DBPlan.cpp
@@ -42,7 +42,7 @@ void DBPlan::meldeKrank(int id) {
         try {
             pqxx::work worker(connectionObject);
             std::string query =
-                "DELETE FROM Veranstalterrrr WHERE ID = $1";
+                "DELETE FROM Veranstalter WHERE ID = $1";
 
             pqxx::result response = worker.exec_params(query, id);
             worker.commit();
@@ -54,3 +54,81 @@ void DBPlan::meldeKrank(int id) {
 
 
 };
+
+void DBPlan::deleteVeranstaltung(int id) {
+    try {
+        pqxx::work worker(connObject);
+        std::string query =
+            "DELETE FROM Veranstaltung WHERE ID = $1;";
+
+        pqxx::result response = worker.exec_params(query, id);
+        worker.commit();
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+}
+
+
+void DBPlan::hinzufuegenVeranstaltung(std::string name, int dauer, std::string ort, std::string raum) {
+    try {
+        pqxx::work worker(connObject);
+        std::string query =
+            "INSERT INTO Veranstaltung (name, dauer, ort, raum) VALUES ($1, $2, $3, $4);";
+        pqxx::result response = worker.exec_params(query, name, dauer, ort, raum);
+        worker.commit();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+}
+
+
+void DBPlan:: hinzufuegenVeranstalter(std::string email, std::string name, std::string pw, bool admin) {
+    try {
+        pqxx::work worker(connObject);
+        std::string query =
+            "INSERT INTO Veranstaltung (email, name, pw, admin) VALUES ($1, $2, $3, $4);";
+        pqxx::result response = worker.exec_params(query, email, name, pw, admin);
+        worker.commit();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+}
+
+
+void DBPlan::deleteStudent(int id) {
+    try {
+        pqxx::work worker(connObject);
+        std::string query =
+            "DELETE FROM studenten WHERE ID = $1;";
+        pqxx::result response = worker.exec_params(query, id);
+        worker.commit();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+}
+
+
+void DBPlan:: hinzufuegenStudent(std::string email, std::string name, std::string pw) {
+
+    try {
+        pqxx::work worker(connObject);
+        std::string query =
+            "INSERT INTO studenten (email, name, pw) VALUES ($1, $2, $3);";
+        pqxx::result response = worker.exec_params(query, email, name, pw);
+        worker.commit();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+
+}
