696f8f724fc68deea747bc45f9fe7b7debfc74ea
Chris-bot374 <chris.moshoevel@stud.hn.de>
2024-07-06 10:10:57 +0200

    Update DBLogin.cpp

--- DBHandler/DBLogin.cpp
+++ DBHandler/DBLogin.cpp
@@ -11,30 +11,31 @@ return 1 if admin | 0 if not admin | -1 if failed
 
 */
 
-int DBLogin::checkValidLogin(int id, std::string pw) {
-    
+int DBLogin::checkValidLogin(std::string id, std::string pw) {
+
     try {
-    pqxx::work worker(connectionObject);
+        pqxx::work worker(connectionObject);
 
-    std::string query =
-        "SELECT admin FROM studenten_veranstalterr WHERE id = $1 AND passwort = $2";
+        std::string query =
+            "SELECT admin FROM studenten_veranstalter WHERE id = $1 AND passwort = $2";
 
-    pqxx::result response = worker.exec_params(query, id, pw);
+        pqxx::result response = worker.exec_params(query, id, pw);
 
-    if (response.affected_rows() > 0) {
+        if (response.affected_rows() > 0) {
 
-        std::cout << response[0][0] << std::endl;
-        if (response[0][0].is_null())
-            return 0;
-        return response[0][0].as<bool>();
+           
+            if (response[0][0].is_null())
+                return 0;
+            return response[0][0].as<bool>();
 
+        }
+
+        return -1;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
-    return -1;
-}
-catch (const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
 }
 
-}
 
